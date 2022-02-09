41a2f147e084d8406b34e98bf2c3966debc69f8e
paolo capellacci <32178361+paolo-capellacci@users.noreply.github.com>
2022-02-09 20:27:27 +0100

    Update test.erl

--- progetto/test.erl
+++ progetto/test.erl
@@ -1,28 +1,41 @@
 -module(test).
--export([test/0, test1/0]).
+-export([start/0, addTS/0, testRD/0, testIN/0]).
 
-
-test() ->
-
-	db:initdb(),
+start() ->
+	db:initdb()
 .
 
-test1() ->
+
+addTS() ->
 
 	db:initdb(),
+	esame:new(ts1),
+	esame:new(ts2),
+	esame:populate(ts1),
+	esame:populate(ts2),
+	node:addNode(ts1, node2@localhost),
+	node:addNode(ts2, node2@localhost),
+	node:addNode(ts1, node1@localhost),
+	node:addNode(ts2, node3@localhost),
+	node:addNode(ts1, node3@localhost)
 
+.
 
+testRD() ->
+	db:initdb(),
 	esame:new(ts1),
 	esame:populate(ts1),
-	esame:new(ts2),
-	esame:populate(ts2),
-
-	esame:look_up(ts1),
-  esame:look_up(ts2)
-
+	esame:out(ts1, {22, 3, 4}),
+	esame:rd(ts1, {22, any, any})
 .
 
-%node:addNode(ts1, node1@localhost).
-%node:addNode(ts1, node2@localhost).
-%node:addNode(ts2, node2@localhost).
-%node:addNode(ts2, node1@localhost).
+testIN() ->
+
+	db:initdb(),
+	esame:new(ts1),
+	esame:populate(ts1),
+	esame:out(ts1, {22, 3, 4}),
+	esame:rd(ts1, {22, any, any})
+.
+
+
